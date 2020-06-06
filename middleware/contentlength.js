ba4d69f436afc3fec9363365f70b14ed639d7471
Carlos Lopez Jr <clshortfuse@gmail.com>
2020-06-06 14:08:43 -0400

    fix(contentlength): allow empty 200 response

--- middleware/contentlength.js
+++ middleware/contentlength.js
@@ -15,10 +15,6 @@ import { addEndObserver, hasEndCalled } from '../utils/writableObserver.js';
  * of just one.
  * @prop {boolean} [overrideHeader=false]
  * Always replace `Content-Length` header
- * @prop {boolean} [forceZero=false]
- * Include `Content-Length=0` on empty responses
- * @prop {boolean} [set204=false]
- * Change status code to 204 if empty and set to 200
  */
 
 /**
@@ -26,13 +22,17 @@ import { addEndObserver, hasEndCalled } from '../utils/writableObserver.js';
  * @param {ContentLengthMiddlewareOptions} [options]
  * @return {MiddlewareFunctionResult}
  */
-function executeContentLengthMiddleware({ res }, options = {}) {
-  let written = false;
+function executeContentLengthMiddleware({ req, res }, options = {}) {
+  if (req.method === 'HEAD') {
+    return 'continue';
+  }
+
   const newWritable = new PassThrough();
   addEndObserver(newWritable);
   const destination = res.replaceStream(newWritable);
   /** @type {Buffer[]} */
   const pendingChunks = [];
+  let length = 0;
 
   /**
    * @param {Buffer} chunk
@@ -44,7 +44,7 @@ function executeContentLengthMiddleware({ res }, options = {}) {
   }
 
   newWritable.on('data', (chunk) => {
-    written = true;
+    length += chunk.length;
     if (hasEndCalled(newWritable)) {
       pendingChunks.push(chunk);
     } else if (options.delayCycle !== false) {
@@ -58,27 +58,27 @@ function executeContentLengthMiddleware({ res }, options = {}) {
   });
 
   newWritable.on('end', () => {
-    if (!written && res.status === 200 && options.set204) {
-      if (!res.headersSent) {
-        res.status = 204;
-      } else {
-        console.warn('Status of 200 sent insead of 204!');
-      }
-    }
-    if (written || options.forceZero) {
-      if (!res.headersSent) {
-        if (options.overrideHeader === true || res.headers['Content-Length'] == null) {
-          res.headers['Content-Length'] = pendingChunks.reduce((prev, curr) => prev + curr.length, 0);
-        } else {
-          console.warn('Content-Length already set!');
+    if (!res.headersSent) {
+      /**
+       * Any response message which "MUST NOT" include a message-body
+       * (such as the 1xx, 204, and 304 responses and any response to a HEAD request)
+       * is always terminated by the first empty line after the header fields,
+       * regardless of the entity-header fields present in the message.
+       * https://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.4
+       */
+      if ((res.status >= 100 && res.status < 200) || res.status === 204 || res.status === 304) {
+        if (options.overrideHeader) {
+          delete res.headers['content-length'];
         }
-      } else if (res.headers['Content-Length'] != null) {
-        console.warn('Content-Length already sent!');
+      } else if (options.overrideHeader === true || res.headers['Content-Length'] == null) {
+        res.headers['Content-Length'] = length;
       }
     }
+
     let chunk;
     // eslint-disable-next-line no-cond-assign
     while (chunk = pendingChunks.shift()) {
+      // TODO: Implement backpressure.
       destination.write(chunk);
     }
     destination.end();
