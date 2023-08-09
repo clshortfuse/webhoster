e22b3a18369c27fb4a5f9e63e569f99a255b62cb
Carlos Lopez Jr <clshortfuse@gmail.com>
2023-08-09 12:44:41 -0400

    test: test node 18 and 20

--- scripts/test-all-sync.sh
+++ scripts/test-all-sync.sh
@@ -1,6 +1,5 @@
 . ~/.nvm/nvm.sh; 
-nvm install 14.15; nvm use 14.15; npx c8 --clean false -r none ava;
-nvm install 14   ; nvm use 14   ; npx c8 --clean false -r none ava;
 nvm install 16.13; nvm use 16.13; npx c8 --clean false -r none ava;
 nvm install 16   ; nvm use 16   ; npx c8 --clean false -r none ava;
-nvm install 17   ; nvm use 17   ; npx c8 --clean false -r none ava;
+nvm install 18   ; nvm use 18   ; npx c8 --clean false -r none ava;
+nvm install 20   ; nvm use 20   ; npx c8 --clean false -r none ava;
--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -1,6 +1,6 @@
 echo . \
-  & sh -c ". ~/.nvm/nvm.sh; nvm install 14.15; nvm use 14.15; npx c8 --clean false -r none ava;" \
-  & sh -c ". ~/.nvm/nvm.sh; nvm install 14   ; nvm use 14   ; npx c8 --clean false -r none ava;" \
   & sh -c ". ~/.nvm/nvm.sh; nvm install 16.13; nvm use 16.13; npx c8 --clean false -r none ava;" \
-  & sh -c ". ~/.nvm/nvm.sh; nvm install 16   ; nvm use 16   ; npx c8 --clean false -r none ava;" \
-  & sh -c ". ~/.nvm/nvm.sh; nvm install 17   ; nvm use 17   ; npx c8 --clean false -r none ava;" \
+  & sh -c ". ~/.nvm/nvm.sh; nvm install 16; nvm use 16; npx c8 --clean false -r none ava;" \
+  & sh -c ". ~/.nvm/nvm.sh; nvm install 18; nvm use 18; npx c8 --clean false -r none ava;" \
+  & sh -c ". ~/.nvm/nvm.sh; nvm install 20; nvm use 20; npx c8 --clean false -r none ava;" \
+  
