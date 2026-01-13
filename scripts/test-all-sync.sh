3758f8ebd478cdfbe02787f9468dff7c0df573f9
Carlos Lopez Jr <clshortfuse@gmail.com>
2026-01-12 19:30:06 -0500

    chore: add nvm 24 tests

--- scripts/test-all-sync.sh
+++ scripts/test-all-sync.sh
@@ -4,3 +4,4 @@ nvm install 16   ; nvm use 16   ; npx c8 --clean false -r none ava;
 nvm install 18   ; nvm use 18   ; npx c8 --clean false -r none ava;
 nvm install 20   ; nvm use 20   ; npx c8 --clean false -r none ava;
 nvm install 22   ; nvm use 22   ; npx c8 --clean false -r none ava;
+nvm install 24   ; nvm use 24   ; npx c8 --clean false -r none ava;
--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -3,5 +3,6 @@ echo . \
   & sh -c ". ~/.nvm/nvm.sh; nvm install 16; nvm use 16; npx c8 --clean false -r none ava;" \
   & sh -c ". ~/.nvm/nvm.sh; nvm install 18; nvm use 18; npx c8 --clean false -r none ava;" \
   & sh -c ". ~/.nvm/nvm.sh; nvm install 20; nvm use 20; npx c8 --clean false -r none ava;" \
-  & sh -c ". ~/.nvm/nvm.sh; nvm install 22; nvm use 20; npx c8 --clean false -r none ava;" \
+  & sh -c ". ~/.nvm/nvm.sh; nvm install 22; nvm use 22; npx c8 --clean false -r none ava;" \
+  & sh -c ". ~/.nvm/nvm.sh; nvm install 24; nvm use 24; npx c8 --clean false -r none ava;" \
   
